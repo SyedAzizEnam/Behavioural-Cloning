71970c5dc83f82e5343cf4d7f83691d260a7ac4b
Syed Aziz Enam <aziz@Syeds-MacBook-Pro.local>
2017-01-23 00:03:13 -0800

    final model

--- drive.py
+++ drive.py
@@ -19,8 +19,9 @@ from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_a
 import tensorflow as tf
 tf.python.control_flow_ops = tf
 
-def preprocess(image):
-    return cv2.resize(cv2.cvtColor(image, cv2.COLOR_RGB2YUV)[40:160,40:280], (200,66))
+def preprocess_input(img):
+    return cv2.resize(cv2.cvtColor(img[60:140,:], cv2.COLOR_RGB2YUV), (66,200))
+
 
 sio = socketio.Server()
 app = Flask(__name__)
@@ -38,13 +39,13 @@ def telemetry(sid, data):
     # The current image from the center camera of the car
     imgString = data["image"]
     image = Image.open(BytesIO(base64.b64decode(imgString)))
-    image_array = np.asarray(image)
-    image_array = preprocess(image_array)
+    image_array = np.asarray(image, dtype=np.float32)
+    image_array = preprocess_input(image_array)
     transformed_image_array = image_array[None, :, :, :]
     # This model currently assumes that the features of the model are just the images. Feel free to change this.
     steering_angle = float(model.predict(transformed_image_array, batch_size=1))
     # The driving model currently just outputs a constant throttle. Feel free to edit this.
-    throttle = 0.2
+    throttle = 0.3
     print(steering_angle, throttle)
     send_control(steering_angle, throttle)
 
--- model.py
+++ model.py
@@ -12,35 +12,76 @@ from keras.optimizers import Adam
 from keras.models import model_from_json
 
 def preprocess(image):
-    return cv2.resize(cv2.cvtColor(image, cv2.COLOR_RGB2YUV)[50:150,40:280], (66,200))
+    return cv2.resize(cv2.cvtColor(image, cv2.COLOR_RGB2YUV),(66,200))
+
+def change_brightness(image):
+    temp = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    brightness = 0.25 + np.random.uniform()
+    temp[:, :, 2] = temp[:, :, 2] * brightness
+    return cv2.cvtColor(temp, cv2.COLOR_HSV2RGB)
+
+def xy_translate(image, angle):
+    x_translation = (100 * np.random.uniform()) - (100 / 2)
+    new_angle = angle + ((x_translation / 100) * 2) * 0.1
+    y_translation = (10 * np.random.uniform()) - (10 / 2)
+    translation_matrix = np.float32([[1, 0, x_translation], [0, 1, y_translation]])
+
+    return cv2.warpAffine(image, translation_matrix, (image.shape[1], image.shape[0])), new_angle
 
 def batch_generator(df, data_path, batch_size, img_rows, img_cols):
+
     num_rows = len(df.index)
-
+    index = np.arange(1,num_rows)
+    EPOCH = 1
     while 1:
-        index = np.random.randint(1,num_rows-1, batch_size//3)
-        batch_files = df.loc[index]
-        train_images = np.zeros((batch_size, img_rows, img_cols, 3))
-        train_steering = np.zeros(batch_size)
-        for i, row in enumerate(batch_files.iterrows()):
+        for j in range(num_rows//batch_size):
+            np.random.shuffle(index)
+            batch_files = df.loc[index[j*batch_size//3:(j+1)*batch_size//3]]
+            train_images = np.zeros((batch_size, img_rows, img_cols, 3))
+            train_steering = np.zeros(batch_size)
+            for i,row in enumerate(batch_files.iterrows()):
 
-            center  = os.path.basename(row[1]['center'])
-            left = os.path.basename(row[1]['left'])
-            right = os.path.basename(row[1]['right'])
+                row = row[1]
+                steering_angle = np.float32(row['steering'])
 
-            center = load_img(data_path+'IMG/'+center)
-            left = load_img(data_path+'IMG/'+left)
-            right = load_img(data_path+'IMG/'+right)
+                center  = os.path.basename(row['center'])
+                center = load_img(data_path+'IMG/'+center)
+                center  = img_to_array(center)[60:140,:]
 
-            center  = preprocess(cv2.cvtColor(img_to_array(center), cv2.COLOR_RGB2YUV))
-            left = preprocess(cv2.cvtColor(img_to_array(left), cv2.COLOR_RGB2YUV))
-            right = preprocess(cv2.cvtColor(img_to_array(right), cv2.COLOR_RGB2YUV))
+                left  = os.path.basename(row['left'])
+                left = load_img(data_path+'IMG/'+left)
+                left  = img_to_array(left)[60:140,:]
 
-            train_images[3*i], train_steering[3*i] = center, np.float32(row[1]['steering'])
-            train_images[3*i+1], train_steering[3*i+1] = left, np.float32(row[1]['steering'])+0.15
-            train_images[3*i+2], train_steering[3*i+2] = right, np.float32(row[1]['steering'])-0.15
+                right  = os.path.basename(row['right'])
+                right = load_img(data_path+'IMG/'+right)
+                right  = img_to_array(right)[60:140,:]
 
-        yield train_images, train_steering
+                flip = np.random.randint(2)
+                if flip:
+                    center  = np.fliplr(img_to_array(center))
+                    left  = np.fliplr(img_to_array(left))
+                    right  = np.fliplr(img_to_array(right))
+
+                brightness_noise = np.random.randint(2)
+                if brightness_noise:
+                    center = change_brightness(center)
+                    left = change_brightness(left)
+                    right = change_brightness(right)
+
+                translate = np.random.randint(2)
+                if translate:
+                    center,steering_angle = xy_translate(center,steering_angle)
+                    left,__ = xy_translate(left,steering_angle)
+                    right,__ = xy_translate(right,steering_angle)
+
+                train_images[3*i], train_steering[3*i] = preprocess(center), -steering_angle if flip else steering_angle
+                train_images[3*i+1], train_steering[3*i+1] = preprocess(left), -(steering_angle+0.15) if flip else steering_angle+0.15
+                train_images[3*i+2], train_steering[3*i+2] = preprocess(right), -(steering_angle-0.15) if flip else steering_angle-0.15
+
+            yield train_images,train_steering
+        EPOCH += 1
+
+
 
 img_rows, img_cols = 200, 66
 data_path = './train_data/'
@@ -54,7 +95,7 @@ df_val = pd.read_csv(val_data_path+'driving_log.csv', names=columns)
 df_val.drop(df_val.index[[0]], inplace=True)
 df_val.dropna(how='any', axis=0, inplace=True)
 """
-train_data = batch_generator(df_train,data_path,batch_size=512,img_rows=img_rows,img_cols=img_cols)
+train_data = batch_generator(df_train,data_path,batch_size=300,img_rows=img_rows,img_cols=img_cols)
 #val_data = batch_generator(df_val,val_data_path,batch_size=1024,img_rows=img_rows,img_cols=img_cols)
 
 model = Sequential()
@@ -66,17 +107,21 @@ model.add(Convolution2D(64, 3, 3, activation='relu', name='Conv4'))
 model.add(Convolution2D(64, 3, 3, activation='relu', name='Conv5'))
 model.add(Flatten())
 model.add(Dense(1164, activation='relu', name='FC1'))
+model.add(Dropout(0.2))
 model.add(Dense(100, activation='relu', name='FC2'))
+model.add(Dropout(0.2))
 model.add(Dense(50, activation='relu', name='FC3'))
+model.add(Dropout(0.2))
 model.add(Dense(10, activation='relu', name='FC4'))
+model.add(Dropout(0.2))
 model.add(Dense(1, name='output'))
 model.summary()
-opt = Adam(lr=0.0001)
+opt = Adam(lr=0.00001)
 model.compile(optimizer=opt, loss='mse', metrics=[])
 
 model.fit_generator(train_data,
-                    samples_per_epoch = 512*50,
-                    nb_epoch=4, verbose=1)
+                    samples_per_epoch = 50000,
+                    nb_epoch=10, verbose=1)
 
 model_json = model.to_json()
 with open('model.json', "w") as json_file:
